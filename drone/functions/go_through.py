21feab89be6dcf142f339e8d006c62dd823bfa16
wcyat <wcyat@wcyat.me>
2023-01-13 22:35:25 +0800

    go through: set common.prev = 1

--- drone/functions/go_through.py
+++ drone/functions/go_through.py
@@ -6,9 +6,7 @@ import type_enforced
 
 @type_enforced.Enforcer
 def go_through(tello: Tello) -> None:
-#    while not (tello.get_distance_tof() - 10) <= common.config['distance']:
-#        tello.move_forward(common.config["distance"])
     obs_height = detect_obs_height(tello)
     if not obs_height:
         return
-    tello.move_forward(common.config["distance"])
+    common.prev = 1
