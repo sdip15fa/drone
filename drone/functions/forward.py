cd4db65e4fcf6911587d10e77ca2c6abaae502cc
wcyat <wcyat@wcyat.me>
2023-01-13 22:35:57 +0800

    fix: forward: remove detect tof distance (direction is not as intended)

--- drone/functions/forward.py
+++ drone/functions/forward.py
@@ -4,8 +4,5 @@ import type_enforced
 
 @type_enforced.Enforcer
 def forward(tello: Tello) -> None:
-    if (tello.get_distance_tof() < common.config['distance']):
-        print(f"TOF distance is less than {common.config['distance']}cm, refusing to move forward.")
-        return
     print(f"move forward {common.config['distance']}cm")
     tello.move_forward(common.config["distance"])
