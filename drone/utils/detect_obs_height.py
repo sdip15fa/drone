e411b1acc358edbd94d0904165611701cbf4f8e0
wcyat <wcyat@wcyat.me>
2023-01-25 23:41:56 +0800

    fix: detect obs height

--- drone/utils/detect_obs_height.py
+++ drone/utils/detect_obs_height.py
@@ -1,25 +1,23 @@
 from threading import Timer
 from djitellopy import Tello
 import drone.common as common
-import type_enforced
+
 from drone.lib import set_interval
 
 obs_height: int = None
 tof: int = 0
 
-@type_enforced.Enforcer
 def get_tof() -> None:
     global tof
     try:
-        if tof <= common.config['max_height'] - 40:
-            return
+        # if tof <= common.config['max_height'] - 40:
+        #    return
         tof = common.tello.get_distance_tof()
         print(f"current tof distance: {tof}")
     except Exception as e:
         print(e)
 
 
-@type_enforced.Enforcer
 def detect_obs_height(tello: Tello) -> int:
     """
     Detects the height of an obstacle.
@@ -40,13 +38,16 @@ def detect_obs_height(tello: Tello) -> int:
     tello.set_speed(10)
     tof_interval: Timer = set_interval(get_tof, 100)
     while (tello.get_height() >= common.config["min_height"]):
-        if tof <= init_tof - 50:
+        if tof <= init_tof - (common.config["max_height"] / 3):
             print(f"height: {init_tof - tof}")
             tof_interval.cancel()
+            tello.go_xyz_speed_mid(
+                0, 0, init_tof - tof, common.config['speed'], common.padId)
+            # tello.move_down(tof)
             break
         tello.move_forward(20)
-    tof_interval.cancel()
-    tello.go_xyz_speed_mid(
-        0, 0, init_height, common.config['speed'], common.padId)
-    tello.move_down(tof)
+    # tof_interval.cancel()
+    # tello.go_xyz_speed_mid(
+    #    0, 0, common.config['max_height'], common.config['speed'], common.padId)
+    # tello.move_down(tof)
     return tof
