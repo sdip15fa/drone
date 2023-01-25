67a74b32f4d2e1f17368f7e0499d1a695241348d
wcyat <wcyat@wcyat.me>
2023-01-25 23:41:27 +0800

    fix: tunnel

--- drone/functions/tunnel.py
+++ drone/functions/tunnel.py
@@ -1,9 +1,11 @@
 from djitellopy import Tello
 import drone.common as common
-import type_enforced
 
 
-@type_enforced.Enforcer
+
+
 def tunnel(tello: Tello) -> None:
-    tello.go_xyz_speed(0, 0, 30, common.config["speed"])
+    # tello.go_xyz_speed(0, 0, 30, common.config["speed"])
+    while tello.get_distance_tof() > 40:
+        tello.move_down(20)
     common.prev = 1
