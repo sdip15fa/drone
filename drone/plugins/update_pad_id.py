a1d5ffb8c22ac9c818853449e5cd02f4711d84bc
wcyat <wcyat@wcyat.me>
2023-02-02 18:32:41 +0800

    plugins(update pad id): add detected pad id for debugging

--- drone/plugins/update_pad_id.py
+++ drone/plugins/update_pad_id.py
@@ -10,6 +10,7 @@ def update_pad_id() -> None:
     if common.end:
         return
     new_pad_id = common.tello.get_mission_pad_id()
+    print(f"detected pad id: {new_pad_id}")
     if new_pad_id in range(1, 9) and common.padId != new_pad_id:
         common.prev = common.padId
         common.padId = new_pad_id
