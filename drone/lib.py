ea3eee98960836febc12e2f5f0eb0c532d96e972
wcyat <wcyat@wcyat.me>
2023-02-02 18:40:01 +0800

    lib: change the implementation of set interval

--- drone/lib.py
+++ drone/lib.py
@@ -1,11 +1,23 @@
+import time
 import threading
+from typing import Callable
+
+StartTime = time.time()
 
 
-def set_interval(func, ms):
-    def func_wrapper():
-        set_interval(func, ms)
-        func()
+class set_interval:
+    def __init__(self, action: Callable, interval: int):
+        self.interval = interval / 1000
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
+        thread.start()
 
-    t = threading.Timer(ms / 1000, func_wrapper)
-    t.start()
-    return t
+    def __setInterval(self):
+        nextTime = time.time() + self.interval
+        while not self.stopEvent.wait(nextTime - time.time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
