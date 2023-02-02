2a9634360e39d5dfc669d633482cab55521a6f17
wcyat <wcyat@wcyat.me>
2023-02-02 18:22:07 +0800

    circle: use circle env variables

--- drone/functions/circle.py
+++ drone/functions/circle.py
@@ -6,10 +6,10 @@ from drone.utils.go_to_pad import go_to_pad
 
 def circle(tello: Tello):
     # tello.curve_xyz_speed(0, 0, 0, 0, 100, 0, common.config["speed"])
-    for _i in range(common.config['around']):
-        tello.move_right(common.config['around_x'])
-        tello.move_forward(common.config['around_y'])
-        tello.move_left(2 * common.config['around_x'])
-        tello.move_back(common.config['around_y'])
-        tello.move_right(common.config['around_x'])
+    for _i in range(common.config['circle']):
+        tello.move_right(common.config['circle_x'])
+        tello.move_forward(common.config['circle_y'])
+        tello.move_left(2 * common.config['circle_x'])
+        tello.move_back(common.config['circle_y'])
+        tello.move_right(common.config['circle_x'])
         go_to_pad(common.padId)
