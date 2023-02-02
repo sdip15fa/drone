f579fe2a9902288fcd99ae3f940eb06f9461e633
wcyat <wcyat@wcyat.me>
2023-02-02 18:22:29 +0800

    functions: snake

---
+++ drone/functions/snake.py
@@ -0,0 +1,8 @@
+from djitellopy import Tello
+import drone.common as common
+def snake(tello: Tello) -> None:
+    tello.move_right(common.config["around_x"])
+    tello.move_forward(common.config["around_y"])
+    tello.move_left(common.config["around_x"] * 2)
+    tello.move_forward(common.config["around_y"])
+    tello.move_right(common.config["around_x"])
