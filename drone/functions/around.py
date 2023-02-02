d809c14caed268888c3dac281d0db7de408015ae
wcyat <wcyat@wcyat.me>
2023-02-02 18:21:47 +0800

    around: use s-shape

--- drone/functions/around.py
+++ drone/functions/around.py
@@ -6,11 +6,16 @@ from drone.utils.go_to_pad import go_to_pad
 
 def around(tello: Tello):
     times = common.config['around']
-    for _i in range(times):
+    for i in range(times):
         if common.padId in range(1, 9):
             if common.change:
                 go_to_pad(common.padId)
         print(f"Going around.")
-        tello.move_right(common.config['around_x'])
-        tello.move_forward(common.config['around_y'])
-        tello.move_left(common.config['around_x'])
+        if i % 2 == 0:
+            tello.move_right(common.config['around_x'])
+            tello.move_forward(common.config['around_y'])
+            tello.move_left(common.config['around_x'])
+        else:
+            tello.move_left(common.config['around_x'])
+            tello.move_forward(common.config['around_y'])
+            tello.move_right(common.config['around_x'])
