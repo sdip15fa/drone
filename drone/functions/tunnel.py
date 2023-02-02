6b8b950d247de7641d42330a6b7ed364d69999cd
wcyat <wcyat@wcyat.me>
2023-02-02 18:23:43 +0800

    tunnel: check previous value to allow use of other directions

--- drone/functions/tunnel.py
+++ drone/functions/tunnel.py
@@ -6,6 +6,9 @@ import drone.common as common
 
 def tunnel(tello: Tello) -> None:
     # tello.go_xyz_speed(0, 0, 30, common.config["speed"])
-    while tello.get_distance_tof() > 40:
-        tello.move_down(20)
-    common.prev = 1
+    while tello.get_distance_tof() > 30:
+        try:
+            tello.move_down(20)
+        except:
+            break
+    common.prev = common.prev if common.prev in range(1, 5) else 1
