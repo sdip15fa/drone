ad99ecbb5d6fd7bf80f2595c144c62247aa576e7
wcyat <wcyat@wcyat.me>
2023-02-07 18:41:08 +0800

    tunnel: remove duplicate code

--- drone/functions/tunnel.py
+++ drone/functions/tunnel.py
@@ -1,14 +1,9 @@
 from djitellopy_reduced import Tello
-import drone.common as common
-
-
 
 
 def tunnel(tello: Tello) -> None:
-    # tello.go_xyz_speed(0, 0, 30, common.config["speed"])
     while tello.get_distance_tof() > 30:
         try:
             tello.move_down(20)
         except:
             break
-    common.prev = common.prev if common.prev in range(1, 5) else 1
