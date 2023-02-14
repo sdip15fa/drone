a5f138f2c1ca63dfa9fdd893ead774a9f265d38d
wcyat <wcyat@wcyat.me>
2023-02-14 17:58:27 +0800

    fix: no recursive looping

--- drone/utils/do_operation.py
+++ drone/utils/do_operation.py
@@ -1,4 +1,5 @@
 import drone.common as common
+from time import sleep
 from drone.utils.get_config import get_config
 from drone.utils.go_to_pad import go_to_pad
 from drone.utils.execute import execute
@@ -21,61 +22,91 @@ from drone.functions.rotate import rotate
 
 
 def do_operation(pad: int) -> None:
+    """
+    The do_operation function is the main function of the program. It takes in a
+    pad number and executes all of the functions that are associated with that pad
+    number. The do_operation function also checks to see if there is a change in 
+    the config file, and if so, it will update its values.
+    
+    :param pad: int: Determine which pad to go to
+    :return: None
+    :doc-author: Trelent
+    """
     common.config = get_config()
 
     if pad in range(1, 9):
         if common.change:
+            # sleep(5)
             go_to_pad(pad)
     else:
         pad = 1
 
+    while pad:
+        pad = match_functions(pad)
+
+
+def match_functions(pad: int) -> int:
+    """
+    The match_functions function is a switch statement that matches the current mode to the corresponding function.
+    The match_functions function also handles switching between modes and pads.
+    
+    :param pad: int: the pad id
+    :return: the next pad id to be run, otherwise 0
+    :doc-author: Trelent
+    """
+    run_pad: int = 0
     match common.config['mode']:
         case 1:
             match pad:
                 case 1:
-                    execute(forward, False)
+                    run_pad = execute(forward, False)
                 case 2:
-                    execute(back, False)
+                    run_pad = execute(back, False)
                 case 3:
-                    execute(left, False)
+                    run_pad = execute(left, False)
                 case 4:
-                    execute(right, False)
+                    run_pad = execute(right, False)
                 case 5:
-                    execute(up, True)
+                    run_pad = execute(up, True)
                 case 6:
-                    execute(down, True)
+                    run_pad = execute(down, True)
                 case 7:
-                    execute(around, True)
-                    # execute(circle, True)
+                    run_pad = execute(around, True)
+                    # rerun_pad = execute(circle, True)
                 case 8:
                     if common.config['switch_mode']:
                         common.config['mode'] = 2
                         common.config['switch_mode'] -= 1
                         common.mode_switched = True
+                        common.pads_permanent = [
+                            1, 2, 4] if common.config['mode'] == 2 else list(range(1, 5))
                     else:
-                        execute(land, True)
+                        run_pad = execute(land, True)
         case 2:
             match pad:
                 case 1:
-                    execute(forward, False)
+                    run_pad = execute(forward, False)
                 case 2:
-                    # execute(back, False)
-                    execute(tunnel, True)
+                    # rerun_pad = execute(back, False)
+                    run_pad = execute(tunnel, True)
                 case 3:
-                    execute(left, False)
+                    run_pad = execute(left, False)
                 case 4:
-                    execute(right, False)
+                    run_pad = execute(right, False)
                 case 5:
-                    execute(go_through, True)
-                    # execute(go_through_predefined_heights, True)
+                    run_pad = execute(go_through, True)
+                    # rerun_pad = execute(go_through_predefined_heights, True)
                 case 6:
-                    execute(rotate, True)
+                    run_pad = execute(rotate, True)
                 case 7:
-                    execute(around, True)
+                    run_pad = execute(around, True)
                 case 8:
                     if common.config['switch_mode']:
                         common.config['mode'] = 1
                         common.config['switch_mode'] -= 1
                         common.mode_switched = True
+                        common.pads_permanent = [
+                            1, 2, 4] if common.config['mode'] == 2 else list(range(1, 5))
                     else:
-                        execute(land, True)
+                        run_pad = execute(land, True)
+    return run_pad
--- drone/utils/execute.py
+++ drone/utils/execute.py
@@ -1,16 +1,26 @@
 import drone.common as common
 from typing import Callable
-import drone.utils.do_operation as do
 
-
-def execute(func: Callable, once: bool) -> None:
+def execute(func: Callable, once: bool) -> int:
+    """
+    The execute function is the main function that runs all of the commands.
+    It takes in a Callable, which is any class or function that has a __call__ method.
+    This allows us to pass in functions and classes as well as single methods. 
+    The execute function also takes an optional once parameter, which defaults to False.
+    
+    :param func: Callable: Pass the function to be executed
+    :param once: bool: Determine whether the function should be executed only once or not
+    :return: The mission pad id to be rerun, otherwise 0
+    :doc-author: Trelent
+    """
     if common.end:
-        return
+        return 0
     if once:
         if not common.executed:
             func(common.tello)
             common.executed = True
         else:
-            do.do_operation(common.prev if common.prev in common.pads_permanent else 1)
+            return common.prev if common.prev in common.pads_permanent else 1
     else:
         func(common.tello)
+        return 0
