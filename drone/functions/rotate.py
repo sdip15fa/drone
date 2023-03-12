ce0b4bb9dbd56c97902e23039c90478ae3bf707d
wcyat <wcyat@wcyat.me>
2023-03-12 22:40:31 +0800

    feat(rotate): use counter clockwise if times > 6

--- drone/functions/rotate.py
+++ drone/functions/rotate.py
@@ -8,14 +8,23 @@ def rotate(tello: Tello) -> None:
     """
     The rotate function tells the drone to rotate clockwise 30 degrees.
     It does this a number of times, as specified in the config file.
-    
+
     :param tello: Tello: Access the tello object
     :return: None
     :doc-author: Trelent
     """
     global runs
-    times = common.config["rotate_times"]
-    for _ in range(times[runs] if runs < len(times) else common.config["rotate_times_default"]):
-        tello.rotate_clockwise(30)
+    times = range(common.config["rotate_times"][runs] if runs < len(
+        common.config["rotate_times"]) else common.config["rotate_times_default"])
+
+    print(f"Rotating {times} times")
+
+    if times > 6:
+        for _ in 12 - times:
+            tello.rotate_counter_clockwise(30)
+    else:
+        for _ in times:
+            tello.rotate_clockwise(30)
+
     common.executed = True
     runs += 1
